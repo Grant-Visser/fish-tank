99bc5e32f6603c53bb68188815c1bb169e76b24c
Grant Visser <grantrvisser@gmail.com>
2014-10-14 11:17:21 +0200

    Create main.cpp

---
+++ Examples/07-29-2014-Classes-More-Cont/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdlib>
+#include"Analyser.h"
+#include "ModeAnalyser.h"
+#include "MeanAnalyser.h"
+
+//We're discussing aggregation and grouping.
+//Protips, all the protips.
+
+//Semester Test 1 Theory
+//Semester Test 2 Practical
+//Final Theory
+
+using namespace std;
+
+int main()
+{
+	Analyser a(10);//Creating Class this in main
+	a.Analyse(); //?? Calling the actual class
+	system("Pause");
+	return 0;
+}
